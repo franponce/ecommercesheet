d761a4538c62b74794ea79e52d52e8d51465b8da
franponce <franponce98@gmail.com>
2022-07-16 04:43:39 -0300

    Se modifica estructura del método parse de Papas

--- product/api.ts
+++ product/api.ts
@@ -13,27 +13,27 @@ export default {
 
     // Lo hacemos async para que entienda que es una promesa
     list: async (): Promise<Product[]> => {
-        return axios.get(
+        return axios
+        .get(
             `https://docs.google.com/spreadsheets/d/e/2PACX-1vRm2aT8iwz6iMY4UXndtWWgbTrsZyDzfCuYRRgbIddF7UvBxJqeWmv-DCFh-OVH0DsrpeJ4RMOF3l5f/pub?gid=0&single=true&output=csv`,
             {
-                responseType: 'blob'
+                responseType: 'blob',
             }
-        ).then((response => {
+        )
+        .then(
+            (response) =>
             // Papaparse no funciona con promise, lo que es medio una cagada si queremos que nuestro método list funcione con promise
 
             // Lo que vamos hacer es usar el constructor de promise, dónde le decimos que devuelve una promesa que es un array de productos 
-            return new Promise<Product[]>((resolve, reject) => {
+             new Promise<Product[]>((resolve, reject) => {
                 Papa.parse(response.data, {
                     // Le pasamos un objeto como parametro header true que lo que hace es indicarle a papa parse que el primer elemento de nuestro array son los headers de la tabla, con eso nos arma el JSON correctamente 
                     header: true,
                     // Y segundo le vamos a pasar una propiedad complete que es una función que tiene los resultados de parsear los objetos en un formato podemos decir rari
-                    complete: (results) => {
-                        return resolve(results.data as Product[]);
-                    },
-                    error: (error) => {
-                        return reject(error.message);
-                    },
-            });
-        });
+                    complete: (results) => resolve(results.data as Product[]),
+                    error: (error) => reject(error.message),
+                    });
+            }),
+        );
     },
 };
